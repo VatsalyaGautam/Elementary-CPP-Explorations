49099bb35db09099eba415557d981be8502f39a8
VatsalyaGautam <147912288+VatsalyaGautam@users.noreply.github.com>
2024-01-18 22:02:04 +0530

    Update AddressValueExample.cpp

--- Pointers/AddressValueExample.cpp
+++ Pointers/AddressValueExample.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 int main()
 {
-    int a=3;int* b;
-    b= &a;
-    std::cout<<"The address of a is:"<<&a<<std::endl;
-    std::cout<<"The address of a is:"<<b<<std::endl;
-    std::cout<<"The value at address"<< b <<  "is:"<< *b <<std::endl;
+    int b=3;int* a;
+    a= &b;
+    std::cout<<"The address of a is:"<<&b<<std::endl;
+    std::cout<<"The address of a is:"<<a<<std::endl;
+    std::cout<<"The value at address"<< a <<  "is:"<< *a <<std::endl;
 
 
     return 0;
